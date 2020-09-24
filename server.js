72a86a7e8bdb5f8314dacc0820784c0585605a50
sandeep7830 <64680878+sandeep7830@users.noreply.github.com>
2020-09-24 21:47:28 +0530

    deployment

--- server.js
+++ server.js
@@ -26,6 +26,6 @@ if (process.env.NODE_ENV==='production') {
 
 
 
-PORT=process.env.PORT ||5000;
+const PORT=process.env.PORT ||5000;
 
 app.listen(PORT,()=>console.log(`sever started at ${PORT}`));
